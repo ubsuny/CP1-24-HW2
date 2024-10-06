acc05f836eeb2e3a643769538fbafa6574a6284b
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-10-06 20:37:58 +0000

    Added a for loop implementation for broadcasting in multiplying an array by a scalar

--- BroadcastingInMultiplication.py
+++ BroadcastingInMultiplication.py
@@ -16,3 +16,16 @@ def scalarMultiplication1(arr, scalar = 0.0):
     result = arr * scalar
     return result
 
+# using a for loop
+def scalarMultiplication2(arr, scalar = 0.0):
+    """
+    Returns the multipliction of an array by a scalar.
+    Parameters:
+    arr (ndarray): Input array
+    Returns:
+    ndarray: The result of multiplying the scalar with each element of the array
+    """
+    result = []
+    for i in range(len(arr)):
+        result.append(arr[i]*scalar)
+    return result
