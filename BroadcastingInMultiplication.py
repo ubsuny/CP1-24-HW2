86ae5b105a8dbf86228e7f4d730cfa95c9805084
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-10-06 20:46:16 +0000

    Added a direct Broadcasting implementation in multiplying two arrays

--- BroadcastingInMultiplication.py
+++ BroadcastingInMultiplication.py
@@ -41,3 +41,21 @@ def scalarMultiplication3(arr, scalar = 0.0):
     """
     result = np.einsum(',...', scalar, arr)
     return result
+
+# Function to multiply two arrays and return the resulting array
+
+# using direct multiplication after broadcasting one of the arrays for its shape to match that of the other
+def arrayMultiplication1(arr1, arr2):
+    """
+    Returns the element-wise multipliction of two arrays.
+    Parameters:
+    arr1 (ndarray), arr2 (ndarray): Input arrays
+    Returns:
+    ndarray: An array of the elements that each of them result from multiplying an element from the first array by an element from the second array
+    """
+
+    broadcasted_arr2 = np.broadcast_to(arr1,arr2.shape)
+
+
+    result = arr1 * broadcasted_arr2 # instead of arr2
+    return result
