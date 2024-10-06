bdeeb0c11b6b51e3281e74631dbf0b909afde34f
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-10-06 20:48:18 +0000

    Added a for-loop implementation for Broadcasting in multiplying two arrays

--- BroadcastingInMultiplication.py
+++ BroadcastingInMultiplication.py
@@ -59,3 +59,21 @@ def arrayMultiplication1(arr1, arr2):
 
     result = arr1 * broadcasted_arr2 # instead of arr2
     return result
+
+# using for loops
+# The issue here is that I'm not able to implement the exact functionality of the broadcasting process
+def arrayMultiplication2(arr1, arr2):
+    """
+    Returns the element-wise multipliction of two arrays.
+    Parameters:
+    arr1 (ndarray), arr2 (ndarray): Input arrays
+    Returns:
+    ndarray: An array of the elements that each of them result from multiplying an element from the first array by an element from the second array
+    """
+
+    # broadcasted_arr2 = np.broadcast_to(arr1,arr2.shape) (no broadcasting took place)
+
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i]*arr2) # that's normal broadcasting directly multiplying a scalar by an array, which means it can be optimized
+    return result
